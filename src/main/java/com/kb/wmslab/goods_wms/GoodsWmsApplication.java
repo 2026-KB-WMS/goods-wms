5dc31a533f6ed1277717ed489c116d9a8c18af76
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-05-07 09:47:05 +0900

    Initial commit

---
+++ src/main/java/com/kb/wmslab/goods_wms/GoodsWmsApplication.java
@@ -0,0 +1,13 @@
+package com.kb.wmslab.goods_wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsWmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kb/wmslab/goods_wms/GoodsWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.wmslab.goods_wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
