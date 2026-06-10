47d1df5c57f91c2731bcce2e47fe5eddb863358f
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-06-10 17:46:59 +0900

    feat: @EntityGraph로 zones 패치 조인하는 findAll 추가

--- src/main/java/com/kb/wmslab/goods_wms/repository/warehouse/WarehouseJpaRepository.java
+++ src/main/java/com/kb/wmslab/goods_wms/repository/warehouse/WarehouseJpaRepository.java
@@ -1,6 +1,13 @@
 package com.kb.wmslab.goods_wms.repository.warehouse;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WarehouseJpaRepository extends JpaRepository<WarehouseJpaEntity, Long> {
+
+    @EntityGraph(attributePaths = {"zones"})
+    @Override
+    List<WarehouseJpaEntity> findAll();
 }
